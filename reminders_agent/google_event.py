e1cfb2466786a0c3370c18d88f82a6a87190e49a
hqzell <hazellow258@gmail.com>
2025-09-18 18:35:18 +0800

    google_event changed to force refresh token by adding access type = offline

--- reminders_agent/google_event.py
+++ reminders_agent/google_event.py
@@ -16,9 +16,8 @@ def get_calendar_service():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json',SCOPES)
+            creds = flow.run_local_server(port=0, access_type="offline", prompt="consent")
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
